c66f086205bd7b1f9640152a888cd2df9777ef40
Conor <cwilson0005@hotmail.co.uk>
2019-02-15 11:30:41 +0000

    changing import/export code in all files

--- src/views/instrument_family_view.js
+++ src/views/instrument_family_view.js
@@ -1,4 +1,4 @@
-const PubSub = require('../helpers/pub_sub.js');
+import PubSub from '../helpers/pub_sub.js';
 
 class InstrumentFamilyView {
   constructor(container) {
@@ -42,7 +42,7 @@ class InstrumentFamilyView {
       listItem.textContent = instrument;
       list.appendChild(listItem);
     });
-    
+
     return list;
   }
 }
